7cd334732377cc3eb5cb3828c89ff901487ea5e8
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-04-03 17:20:43 +0100

    incomplete solution for leetcode - 5.longest-palindromic-substring

---
+++ leetCode/__tests__/5.longest-palindromic-substring.test.js
@@ -0,0 +1,40 @@
+const longestPalindrome = require('../katas/5.longest-palindromic-substring');
+
+describe('longestPalindrome', () => { 
+    test('input "babad" should return "bab" or "aba"', () => { 
+        const input = 'babad';
+        expect(longestPalindrome(input)).toMatch(/(bab|aba)/i)
+     })
+    test('input "cbadd" should return "dd"', () => { 
+        const input = 'cbadd';
+        expect(longestPalindrome(input)).toBe('dd')
+     })
+    test('input "cbbd" should return "bb"', () => { 
+        const input = 'cbbd';
+        expect(longestPalindrome(input)).toBe('bb')
+     })
+     test('input "ac" should return "a"', () => { 
+        const input = 'ac';
+        expect(longestPalindrome(input)).toBe('a')
+     })
+     test('input "bb" should return "bb"', () => { 
+        const input = 'bb';
+        expect(longestPalindrome(input)).toBe('bb')
+     })
+     test('input "aacabdkacaa" should return "aca"', () => { 
+        const input = 'aacabdkacaa';
+        expect(longestPalindrome(input)).toBe('aca')
+     })
+     test('input long string should return', () => { 
+        const input = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+        expect(longestPalindrome(input)).toBe('ranynar')
+     })
+     test('input long string should return', () => { 
+        const input = "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy";
+        expect(longestPalindrome(input)).toBe('fklkf')
+     })
+     test('input long string should return', () => { 
+        const input = "jglknendplocymmvwtoxvebkekzfdhykknufqdkntnqvgfbahsljkobhbxkvyictzkqjqydczuxjkgecdyhixdttxfqmgksrkyvopwprsgoszftuhawflzjyuyrujrxluhzjvbflxgcovilthvuihzttzithnsqbdxtafxrfrblulsakrahulwthhbjcslceewxfxtavljpimaqqlcbrdgtgjryjytgxljxtravwdlnrrauxplempnbfeusgtqzjtzshwieutxdytlrrqvyemlyzolhbkzhyfyttevqnfvmpqjngcnazmaagwihxrhmcibyfkccyrqwnzlzqeuenhwlzhbxqxerfifzncimwqsfatudjihtumrtjtggzleovihifxufvwqeimbxvzlxwcsknksogsbwwdlwulnetdysvsfkonggeedtshxqkgbhoscjgpiel";
+        expect(longestPalindrome(input)).toBe('sknks')
+     })
+ })
---
+++ leetCode/katas/5.longest-palindromic-substring.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=5 lang=javascript
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    const strArray = s.split('');
+    let result = [];
+
+    const checkPalindrome = (arr) =>{
+        if (arr.join('') === arr.reverse().join('') && arr.length > result.length){
+            result = arr.join('');
+            return true;
+        }
+    }
+
+    if (s.length === 2 && checkPalindrome(strArray)) return result;
+
+    for (let m = strArray.length; m > 0; m--) {
+        for (let o = 0; o < strArray.length; o++) {
+            checkPalindrome(strArray.slice(o,m));
+        }
+   }
+
+   return result;
+};
+// @lc code=end
+
+module.exports = longestPalindrome;
