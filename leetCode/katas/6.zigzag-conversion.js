99cd3670a970dc5bb277089e31e7c8c3af78e867
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-04-15 16:01:28 +0100

    solution for leetcode - 6.zigzag-conversion

--- leetCode/katas/6.zigzag-conversion.js
+++ leetCode/katas/6.zigzag-conversion.js
@@ -11,62 +11,54 @@
  * @return {string}
  */
 var zigZag = function(s, numRows) {
-    const newS = s.split('');
-    const hold = [
-        // ['P',' ','A',' ','H',' ','N'],
-        // ['A','P','L','S','I','I','G'],
-        // ['Y',' ','I',' ','R',' ',' ']
-        //"00P 10A 20Y 11P 02A 12L 22I 13S 04H 14I 24R 15I 06N 16G"
-        //"00P 01A 02Y 11P 20A 21L 22I 31S 40H 41I 42R 51I 60N 61G"
-        /*
-        row = 3
-        length = 14
-        3*14 = 42 / 2 = 21
-        7 for each row
-        7 * 3 = 21
-        21 / 3 = 21
 
+    if (numRows < 2) return s;
 
-        row = 4
-        length = 14
-        4*14 = 56 / 2 = 28
-        7 for each row
-        7 * 4 = 28
-        28 / 4 = 7
-
-        1 2 3 4 5 6 7 8 9 10 11 12 13 14
-        0 1 2 3 4 5 6 7 8 9  10 11 12 13
-        P A Y P A L I S H I  R  I  N  G
-          2   3   5
-
-        P   A   H   N
-        A P L S I I G
-        Y   I   R
-        */ 
-    ];
-
+    const hold = [], str = s.split('');
+   
     const spaces = ((numRows*s.length)/2)/numRows;
     for (let i = 0; i < numRows; i++) {
         hold.push([]);
         for (let o = 0; o < spaces; o++) {
-            hold[i].push(o);
+            hold[i].push('-');
         }
     };
 
-    for (let r = 0; r < spaces; r++) {
-        for (let e = 0; e < hold.length; e++) {
-            console.log(e, hold[e][r])
+    let column = 1, between = 0;
+    for (let col = 0; col < spaces; col++) {
+
+        for (let rowD = 0; rowD < hold.length; rowD++) {
+            
+            if (column === 1){
+                hold[rowD][col] = str.shift();
+            } else {
+                if (between === rowD){
+                    hold[rowD][col] = str.shift();
+                }
+            }
+
+            if (rowD === hold.length-1){
+                if (between <= 0){
+                    between = rowD-1;
+                } else {
+                    between--;
+                }
+            }
+
+        }
+
+        if (column === numRows-1){
+            column = 1;
+        } else {
+            column++;
         }
     }
 
+    const word = hold.map((row)=> {
+        return row.filter(val => val !== '-').join('');
+    })
 
-    // for (let o = 0; o < s.length; o++) {
-    //     const element = array[o];
-    // }
-
-    // console.log(hold)
-    // console.log(s.length)
-    // console.log(numRows)
+    return word.join('')
 };
 
 // @lc code=end
