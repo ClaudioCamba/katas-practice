89e6fe0416e198d62367ec6d5089f4a77c1040ae
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-07-31 22:22:31 +0100

    solution for leetcode - 806.number-of-lines-to-write-string

--- leetCode/__tests__/806.number-of-lines-to-write-string.test.js
+++ leetCode/__tests__/806.number-of-lines-to-write-string.test.js
@@ -1,3 +1,10 @@
 const numberOfLines = require('../katas/806.number-of-lines-to-write-string');
 
-
+describe('numberOfLines', () => { 
+    test('inputs of nums = [2,7,11,15], target = 9 should return [0, 1]', () => { 
+        const widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], 
+        s = "abcdefghijklmnopqrstuvwxyz",
+        result = [3,60];
+        expect(numberOfLines(widths,s)).toEqual(result);
+     });
+ })
--- leetCode/katas/806.number-of-lines-to-write-string.js
+++ leetCode/katas/806.number-of-lines-to-write-string.js
@@ -11,7 +11,40 @@
  * @return {number[]}
  */
 var numberOfLines = function(widths, s) {
-    
+    const alphabet = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+      ];
+      
+    const letters = {};
+    let totalWidth = [], lines = 0, lastWidth = 0, hold = 0;
+
+
+    for (let i = 0; i < widths.length; i++) {
+        // console.log(widths[i])
+        // console.log(s[i])
+        letters[alphabet[i]] = widths[i]
+    }
+
+    for (let o = 0; o < s.length; o++) {
+        if (hold + letters[s[o]] > 100){
+            hold = 0;
+            lines += 1;
+        }
+
+        if (hold + letters[s[o]] <= 100){
+            hold += letters[s[o]];
+        }
+
+        if (s.length-1 === o && hold > 0){
+            lines += 1;
+            lastWidth = hold;
+        }
+
+    }
+
+    return [lines, lastWidth]
 };
 // @lc code=end
 
+module.exports = numberOfLines;
